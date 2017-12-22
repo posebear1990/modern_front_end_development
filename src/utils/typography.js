f9aaff6d392ababe5c853cec47ae91af1fc3e165
peixiong.gao <peixiong.gao@hypers.com>
2017-12-22 18:48:47 +0800

    add project structure

---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography";
+import githubTheme from "typography-theme-github"
+console.log(githubTheme);
+const typography = new Typography(githubTheme);
+
+export default typography;
